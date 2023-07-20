b0069a0e8678fb680f03e84216fb2c794319757c
Pierre Slagbrand <pierre.slagbrand@hotmail.com>
2023-07-20 19:30:33 +0000

    Add contact page

--- blog/models.py
+++ blog/models.py
@@ -50,8 +50,10 @@ class Comment(models.Model):
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField()
     subject = models.CharField(max_length=200)
     message = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.subject
--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
+    path('contact/', views.contact_us, name='contact_us'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
